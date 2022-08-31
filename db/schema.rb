c2fdda25f62d3a78bfcd029a6821513797d906de
Johanna Bjornsdottir <johannaeir@simnet.is>
2022-08-31 11:59:46 +0200

    assigned tasks basic function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_131953) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_31_075503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_131953) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "meals", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.string "picture_url"
+    t.bigint "account_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_meals_on_account_id"
+  end
+
   create_table "profile_tasks", force: :cascade do |t|
     t.bigint "profile_id", null: false
     t.boolean "done", default: false
@@ -89,6 +99,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_131953) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "meals", "accounts"
   add_foreign_key "profile_tasks", "profiles"
   add_foreign_key "profile_tasks", "task_templates"
   add_foreign_key "profiles", "accounts"
