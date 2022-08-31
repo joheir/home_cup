bd5b84d67ae1e5df32fecb93d632d4c0df0e2174
Yami Pedroso-AI <yamirovinci@gmail.com>
2022-08-31 10:06:09 +0200

    clear

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+5.times do |meal|
+    Meal.create!(name: "Meal #{meal}")
+end
