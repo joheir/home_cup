056065dc2eba58e59c765e9714f55a5acebfec62
Johanna Bjornsdottir <johannaeir@simnet.is>
2022-09-01 15:09:05 +0200

    added progress bars on dashboard and calculated total progress

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import ProgressController from "./progress_controller"
+application.register("progress", ProgressController)
