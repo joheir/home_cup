ab217c103129e845b8cd9c63b2df6f3a9956f139
Johanna Bjornsdottir <johannaeir@simnet.is>
2022-09-02 14:55:56 +0200

    fixed packing page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def conditional_p(value)
+    return if value.nil?
+
+    content_tag(:p, value.capitalize, {})
+  end
+
+  def show_all_packing_items(packing)
+    attributes = packing.attributes
+    item_count = attributes.keys.grep(/item/).count
+    result = (1..item_count).map do |i|
+      conditional_p(attributes["item#{i}"])
+    end
+    result.compact.join.html_safe
+  end
 end
