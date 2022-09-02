9406503fee91a81535fd2f4f51da9c2e452778b6
Johanna Bjornsdottir <johannaeir@simnet.is>
2022-09-02 12:51:30 +0200

    loops work on dashboard

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,6 +4,16 @@ class Account < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :profiles
+  has_many :profile_tasks, through: :profiles
   has_many :task_templates
   has_many :meals
+
+  def done_tasks_count
+    return 0 if task_templates.empty?
+
+    done_tasks_total = profiles.profile_tasks.where(done: true).count
+    return 0 if done_tasks_total.zero?
+
+    (done_tasks_total.to_f/task_templates.count) * 100
+  end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,12 +6,11 @@ class Profile < ApplicationRecord
   has_many :profile_tasks
 
   def progress
-    profile_tasks = ProfileTask.where(profile_id: id)
-    count = 0
-    profile_tasks.each do |profile_task|
-      profile_task.done? ? count += 1 : count
-    end
+    return 0 if profile_tasks.empty?
 
-    (count.to_f / profile_tasks.length) * 100
+    done_tasks = profile_tasks.where(done: true).count
+    return 0 if done_tasks.zero?
+
+    (done_tasks.to_f / profile_tasks.count) * 100
   end
 end
