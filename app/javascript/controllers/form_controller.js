08eb622974ea000219944ac44d18e3250dcc3076
Johanna Bjornsdottir <johannaeir@simnet.is>
2022-09-04 21:47:32 +0200

    added js to bags form

---
+++ app/javascript/controllers/form_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    console.log("Hello!")
+  }
+
+}
+
+const displayNew = () => {
+  const field = document.querySelector(`.active`);
+  if (field.nextElementSibling) {
+    field.nextElementSibling.classList.remove('.d-none');
+    field.nextElementSibling.classList.add('.active');
+    field.classList.remove('active');
+  }
+};
+
+const displayField = (event) => {
+  if (event) {
+    displayNew;
+  }
+};
+
+document.addEventListener("keyup", displayField);
