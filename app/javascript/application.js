2b9a3f715d19919ea8abfe9a562e9ccd2c0b2b71
Johanna Bjornsdottir <johannaeir@simnet.is>
2022-09-06 17:04:35 +0200

    hopefully fixed

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,6 +3,4 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
 import "./lib/item_input"
-import {renderItemInput} from "./lib/item_input"
 //= require turbolinks
-renderItemInput()
