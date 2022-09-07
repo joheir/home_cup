0eeaf586c84daff5b96ccdef85228f5f0e9e6d94
Yami Pedroso-AI <yamirovinci@gmail.com>
2022-09-07 16:45:43 +0200

    Finishe bags colors

--- app/models/packing.rb
+++ app/models/packing.rb
@@ -1,4 +1,12 @@
 class Packing < ApplicationRecord
   belongs_to :profile
   validates :name, presence: true
+
+  COLORS = {
+    Darkorange: "#FD703F",
+    Peach: "#F27877",
+    Greenblue: "#4EB9B3",
+    Darkblue: "#27357E",
+    Yellow: "#FFA963"
+  }
 end
